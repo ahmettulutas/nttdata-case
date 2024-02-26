5c0f235ad8c874970a0a0f9c4343882cd82628e3
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-02-26 17:37:28 +0300

    asda

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  /* const [count, setCount] = useState(0) */
+
+  return (
+    <>
+      <p className="text-lg bg-red-300 p-4">hello</p>
+    </>
+  );
+}
+
+export default App;
