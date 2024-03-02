d0988c6c1b2057e025395d2ec283c641ec75c8b9
ahmetulutas <ahmet.ulutas@ecozum.com>
2024-03-02 09:24:43 +0300

    fix: navbar toggle animation bugfix

--- src/components/layouts/navbar/navbar-toggle.tsx
+++ src/components/layouts/navbar/navbar-toggle.tsx
@@ -9,7 +9,7 @@ export const NavbarToggle: React.FC<ToggleProps> = ({ open, toggle }) => (
       className={`${open ? 'transform rotate-[45deg] translate-x-[-2px] translate-y-[10px]' : ''} bg-black h-0.5 w-6 transition-all duration-200 ease-in`}
     />
     <div
-      className={`${open ? 'w-0' : 'w-full'} bg-black h-0.5 w-6 transition-all duration-200 ease-in mt-1`}
+      className={`${open ? 'w-0' : 'w-full'} bg-black h-0.5 transition-all duration-200 ease-in mt-1`}
     />
     <div
       className={`${open ? 'transform rotate-[-45deg] translate-x-[-2px] translate-y-[0px]' : ''} bg-black h-0.5 w-6 transition-all duration-200 ease-in mt-1`}
